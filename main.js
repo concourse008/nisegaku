2dbe83153f988edee9be40c7295aa2b0e163932e
concourse008 <61238161+concourse008@users.noreply.github.com>
2020-07-16 19:20:39 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,10 @@
 const game0Divided = document.getElementById('game-area0');
 const game1Divided = document.getElementById('game-area1');
 //ブーメラン位置
-let bumex = 230;
+let bumex = 215;
 let bumey = 50;
 let bumez = 0;
+let baloon00 = [400, 400, 400, 400, 400]//X値
 
 //画像読み込み
 const canvas = {
@@ -21,9 +22,10 @@ let ctx = canvas[flip].getContext('2d');
 const ctx0 = canvas[2].getContext('2d');
 const srcs = [//画像一覧
   ['back.png', 0, 0],
-  ['gaku.png', 170, 20],
+  ['gaku.png', 155, 20],
   ['bume.png', bumex, bumey],
-  ['front.png', 0, 0]
+  ['front.png', 0, 0],
+  ['baloon0.png', 155, 250]
 ];
 let images = [];
 for (let i in srcs) {
@@ -54,21 +56,21 @@ function step() {
   ctx0.clearRect(0, 0, 400, 400);
   ctx.drawImage(images[0], srcs[0][1], srcs[0][2]);
   ctx.drawImage(images[1], srcs[1][1], srcs[1][2]);
-  
-  //translateメソッド前の設定を保存
   ctx0.save();
-  ctx0.translate(parseInt(bumex), parseInt(bumey)); 
+  ctx0.translate(parseInt(bumex), parseInt(bumey));
   ctx0.rotate((bumez * Math.PI) / 180);
   ctx0.drawImage(images[2], -13, -30);
-  //translateメソッド前の設定に戻す
   ctx0.restore();
+  for (let i = 0; i < 5; i++) {
+    ctx.drawImage(images[4], baloon00[i], i * 35 + 220);//風船０
+  }
   ctx.drawImage(images[3], srcs[3][1], srcs[3][2]);
 }
-
 step();
 //画面の描写
 
 //クリック
+let clickok = true;
 let point = 0;
 canvas[2].addEventListener('click', e => {
   //マウスの座標をカンバス内の座標と合わせる
@@ -77,30 +79,60 @@ canvas[2].addEventListener('click', e => {
     x: e.clientX - rect.left,
     y: e.clientY - rect.top
   }
-  rolestart();
+  if (clickok) {
+    clickok = false;
+    rolestart();
+  } else { }
 })
 let back = false;
-function role(){
-  if (back){
-  bumex = bumex + 0;
-  bumey = bumey - 5;
-  bumez = bumez + 33; 
-  }else{
+function role() {
+  if (back) {
+    bumex = bumex + 0;
+    bumey = bumey - 5;
+    bumez = bumez + 33;
+  } else {
     bumey = bumey + 5;
     bumez = bumez + 33;
   }
-  if (bumey > 400){
+  if (bumey > 400) {
     back = true;
-  }else if (bumey == 50){
+  } else if (bumey == 50) {
     back = false;
     bumez = 0;
     clearInterval(moveing);
+    clickok = true;
   }
 }
 let moveing = 0;
-function rolestart(){
-  function roleng(){
-    moveing = setInterval(role,1000/30);
+function rolestart() {
+  function roleng() {
+    moveing = setInterval(role, 1000 / 30);
   }
   roleng();
 }
+
+//風船の移動
+let lastgo = 0;
+function bal0() {
+  for (let i in baloon00) {
+    if (baloon00[i] == 400) {
+      if (Math.floor(Math.random() * 30) === 0 && lastgo >= 45) {
+        baloon00[i] = baloon00[i] - 10;
+        lastgo = 0;
+      }
+      lastgo++;
+    } else if (baloon00[i] <= -15) {
+      baloon00[i] = 400;
+    } else {
+      baloon00[i] = baloon00[i] - 2;
+    }
+  }
+}
+let balmoveing = 0;
+function baloonstart() {
+  function balmove() {
+    balmoveing = setInterval(bal0, 1000 / 30);
+  }
+  balmove();
+}
+baloonstart();
